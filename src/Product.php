f5fb8cd582a991a3e9a78ad352bf84acd324ab82
Carl Whittick <carlwhittick@gmail.com>
2025-01-31 06:10:52 +0000

    Define basic product structure

--- src/Product.php
+++ src/Product.php
@@ -4,5 +4,15 @@ namespace App;
 
 class Product
 {
-
+    public function __construct(
+        protected string $title,
+        protected float $price,
+        protected string $imageUrl,
+        protected int $capacityMB,
+        protected string $colour,
+        protected string $availabilityText,
+        protected bool $isAvailable,
+        protected string $shippingText,
+        protected string $shippingDate,
+    ) {}
 }
