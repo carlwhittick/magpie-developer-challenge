39c9695c53e4e28151c547a7c205f1343b0f157a
Carl Whittick <carlwhittick@gmail.com>
2025-02-01 20:52:47 +0000

    Improve comments

--- src/Classes/Product.php
+++ src/Classes/Product.php
@@ -8,6 +8,22 @@ use Carbon\Carbon;
 
 class Product implements \JsonSerializable
 {
+    /**
+     * Product constructor.
+     * 
+     * Initializes the product's details, including its title, price, image URL, capacity,
+     * color, availability, and shipping information.
+     *
+     * @param string $title The product's title.
+     * @param Cost $price The product's price, wrapped in a Cost object.
+     * @param string $imageUrl The full URL to the product's image.
+     * @param FileSize $capacity The product's capacity, wrapped in a FileSize object.
+     * @param string $colour The product's color.
+     * @param string $availabilityText The product's availability text.
+     * @param bool $isAvailable Whether the product is available or not.
+     * @param string $shippingText Text providing shipping information.
+     * @param Carbon|null $shippingDate The date when the product will ship, or null if unavailable.
+     */
     public function __construct(
         protected readonly string $title,
         protected readonly Cost $price,
@@ -20,6 +36,15 @@ class Product implements \JsonSerializable
         protected readonly Carbon|null $shippingDate,
     ) {}
 
+    /**
+     * Serializes the product into an array for JSON output.
+     *
+     * Converts the product's properties into a format that can be easily represented as JSON.
+     * It includes essential properties such as title, price, capacity in MB, color, availability, 
+     * and optional shipping details if available.
+     *
+     * @return array The serialized product data.
+     */
     public function jsonSerialize(): array
     {
         $data = [
